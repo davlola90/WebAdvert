2305cc41ecb1b459ba982b00e10579abde56dc4b
Aymen Toukabri <aymen.toukabri@corilus.be>
2020-04-04 20:43:38 +0100

    Ajoutez des fichiers projet.

---
+++ WebAdvert.Web/Models/Accounts/SignupModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAdvert.Web.Models.Accounts
+{
+    public class SignupModel
+    {
+
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [StringLength(6,ErrorMessage ="password must be at least six characters long")]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare("Password",ErrorMessage ="Password and its confirmation do not match")]
+        public string ConfirmPassword { get; set; }
+    }
+}
